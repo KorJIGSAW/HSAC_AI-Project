2376c67c0c8529658934d61aee267b618f6850c4
KorJIGSAW <111522731+KorJIGSAW@users.noreply.github.com>
2024-05-10 03:59:20 +0900

    table로 변경

--- src/components/commons/calendar/calendar.jsx
+++ src/components/commons/calendar/calendar.jsx
@@ -2,42 +2,22 @@ import React from 'react';
 import styled from 'styled-components';
 
 // 스타일 컴포넌트 정의
-const ScheduleContainer = styled.div`
-  display: flex;
+const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse; /* 테이블 셀 사이의 간격을 없앱니다. */
 `;
 
-const DayColumn = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  border-right: 1px solid #f0f0f0;
-`;
-
-const TimeSlot = styled.div`
-  flex: 1;
-  border: 1px solid #f0f0f0;
+const TableCell = styled.td`
+  border: 1px solid #f0f0f0; /* 셀마다 구분선을 추가합니다. */
+  text-align: center;
   padding: 10px;
-  text-align: center;
-  min-height: 50px;
 `;
 
-const Hour = styled.div`
-  background-color: #f9f9f9;
+const TableHeader = styled.th`
+  border: 1px solid #f0f0f0; /* 헤더 셀마다 구분선을 추가합니다. */
+  text-align: center;
   padding: 10px;
-  text-align: center;
-  border-bottom: 1px solid #f0f0f0;
-`;
-
-const HoursContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  background-color: #f0f0f0;
-`;
-
-const DayLabel = styled.div`
-  min-width: 50px;
-  text-align: center;
-  font-weight: bold;
+  background-color: #f9f9f9; /* 배경색을 추가합니다. */
 `;
 
 const scheduleData = {
@@ -48,37 +28,31 @@ const scheduleData = {
   "금": {"오전 9": "정치학", "오전 11": "문학"},
 };
 
+const hours = ["08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00"];
 
-// scheduleData 및 hours는 변경 없음
-const hours = ["08:00", "08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00"];
-
-export default function Calendar() {
+export default function CalendarTable() {
   return (
-    <ScheduleContainer>
-      {Object.keys(scheduleData).map(day => (
-        <DayColumn key={day}>
-          <DayLabel>{day}</DayLabel> {/* 각 요일을 표시 */}
-          {hours.map((hour, index) => {
-            // 해당 시간에 맞는 수업 정보를 찾아 표시합니다.
-            const classInfo = scheduleData[day][hour];
-            return (
-              <TimeSlot key={index}>
-                {classInfo || ''}
-              </TimeSlot>
-            );
-          })}
-        </DayColumn>
-      ))}
-      <HoursContainer>
-        <DayLabel>시간</DayLabel> {/* 요일 표시를 위한 레이블 추가 */}
+    <Table>
+      <thead>
+        <tr>
+          <TableHeader>시간/요일</TableHeader>
+          {Object.keys(scheduleData).map(day => (
+            <TableHeader key={day}>{day}</TableHeader>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
         {hours.map((hour, index) => (
-          <Hour key={index}>{hour}</Hour>
+          <tr key={index}>
+            <TableCell>{hour}</TableCell>
+            {Object.keys(scheduleData).map(day => {
+              const hourKey = hour === "09:00" ? "오전 9" : hour === "11:00" ? "오전 11" : hour;
+              const classInfo = scheduleData[day][hourKey];
+              return <TableCell key={day}>{classInfo || ''}</TableCell>;
+            })}
+          </tr>
         ))}
-      </HoursContainer>
-    </ScheduleContainer>
+      </tbody>
+    </Table>
   );
 }
-
-
-
-
