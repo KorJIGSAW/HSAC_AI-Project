ebe01802ae63acc7f506fd369228c543c1e41f59
JIGSAW <bm838366@gmail.com>
2024-05-24 13:47:33 +0900

    Container안에 Calendar삽입

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import CardList from 'components/commons/cardList/cardList';
-import Calendar from 'components/commons/calendar/calendar';
+import CalendarList from 'components/commons/calendarList/calendarList';
 import routes from 'utils/constants/routes';
 
 function App() {
   return (
     <Routes>
       <Route path={routes.minseo} element={<CardList />}></Route>
-      <Route path={routes.jongbeom} element={<Calendar />}></Route>
+      <Route path={routes.jongbeom} element={<CalendarList />}></Route>
     </Routes>
   );
 }
